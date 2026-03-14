35ff704d45964d9bcb053ddcbf3d99d95a86da9d
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-14 13:37:52 +0600

    add required environment variables for better authentication and app configuration

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -9,10 +9,22 @@ interface EnvConfig {
     NODE_ENV: string;
     PORT: string;
     DATABASE_URL: string;
+    BETTER_AUTH_SECRET: string;
+    BETTER_AUTH_URL: string;
+    APP_URL: string;
+    FRONTEND_URL: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requireEnvVariable = ["NODE_ENV", "PORT", "DATABASE_URL"];
+    const requireEnvVariable = [
+        "NODE_ENV",
+        "PORT",
+        "DATABASE_URL",
+        "BETTER_AUTH_SECRET",
+        "BETTER_AUTH_URL",
+        "APP_URL",
+        "FRONTEND_URL",
+    ];
 
     requireEnvVariable.forEach((variable) => {
         if (!process.env[variable]) {
@@ -30,6 +42,10 @@ const loadEnvVariables = (): EnvConfig => {
         NODE_ENV: process.env.NODE_ENV as string,
         PORT: process.env.PORT as string,
         DATABASE_URL: process.env.DATABASE_URL as string,
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+        APP_URL: process.env.APP_URL as string,
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
     };
 };
 
