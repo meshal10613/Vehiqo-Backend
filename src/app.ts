b6515e9eb49c0eec2b8e90006dd092a3a1e2aa34
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2026-03-14 14:07:02 +0600

    implement global error handling and add middleware for Zod and Prisma errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import express, { Application, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import { IndexRoutes } from "./app/routes";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express();
+
+//? Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+//? Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1", IndexRoutes);
 
 app.get("/", async (req: Request, res: Response) => {
     res.send("Hello World!");
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
